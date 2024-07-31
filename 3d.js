d42725bfc30cbc35f3412b327fd455700261c864
kiaantolia <kiaantolia@gmail.com>
2024-07-31 11:03:31 -0700

    Update 3d.js

--- 3d.js
+++ 3d.js
@@ -1,4 +1,7 @@
-export function docs(){
-  document.innerHTML = "hellow world";
-  console.log("hewwo")
+function docs(){
+  document.body.innerHTML = "hellow world";
+  console.log("hewwo");
 }
+
+// Attach the function to the global window object
+window.docs = docs;
